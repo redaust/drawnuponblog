8ef4f36fcfb9b0fd03e6f34777565c719e056179
Austin Marsh <redausty@live.com>
2023-08-03 21:56:05 -0400

    adding authentication

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,5 +1,8 @@
 class BlogPostsController < ApplicationController
+
+before_action :authenticate_user!, except: [:index, :show]
 before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
+
 	def new
 		@blog_post = BlogPost.new
 	end
