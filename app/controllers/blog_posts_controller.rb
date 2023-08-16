1e136515ff9ff6625c43507c86b20f5d6edf2359
Austin Marsh <redausty@live.com>
2023-08-15 21:16:14 -0400

    adding cover image object

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -51,7 +51,7 @@ before_action :set_blog_post, only: [:show, :edit, :update, :destroy]
 	private
 
 	def blog_post_params
-		params.require(:blog_post).permit(:title, :content, :published_at)
+		params.require(:blog_post).permit(:title, :content, :cover_image, :published_at)
 	end
 
 	def set_blog_post
